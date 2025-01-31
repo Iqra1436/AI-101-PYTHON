a5e220d894ff41b65d92f4ab9fa3cac32b80997b
IQRA1436 <iqrajahangir698@gmail.com>
2025-01-31 13:53:24 +0500

    add first 2 classes

---
+++ Class_01(Data_Types)/Data_Types.py
@@ -0,0 +1,51 @@
+# 1. String (str) (text)
+
+name = "Iqra Jahangir"
+print(name) # print
+print(type(name)) # type
+print(id(name)) # physical address
+# print(dir(name)) 
+# print([i for i in dir(name) if "__" not in i ]) # methods & attributes
+
+# 2.  Int (Number)
+
+num : int = 100
+print(num) # print
+print(type(num)) # type 
+print(id(num)) # physical address
+# print(dir(num)) 
+# print([i for i in dir(name) if "__" not in i ]) # methods & attributes
+
+# 3. Float (Decimal Number)
+
+f : float = 2.5
+print(f) # print
+print(type(f)) # type
+print(id(f)) # physical address
+# print(dir(f)) 
+# print([i for i in dir(name) if "__" not in i ]) # methods cls
+# & attributes
+
+# 4. Boolean (Bool) (True , False)
+
+b : bool = True
+print(b) # print
+print(type(b)) # type
+# print(dir(b)) 
+# print([i for i in dir(name) if "__" not in i ]) # methods & attributes
+
+# 5.List
+
+l : list[str] = ["Iqra", "Jahangir"]
+print(l) # print
+print(type(l)) # type
+# print(dir(l)) 
+# print([i for i in dir(name) if "__" not in i ]) # methods & attributes
+
+# 6. Tupple
+
+tup : tuple[str,int,float] = ("a",34,2.5)
+print(tup) # print
+print(type(tup)) # type
+# print(dir(tup)) 
+# print([i for i in dir(name) if "__" not in i ]) # methods & attributesh
---
+++ Class_01(Data_Types)/readme.md
@@ -0,0 +1,8 @@
+step_01 
+conda create -n myenv python==3.12 -y
+step_02
+conda activate myenv
+step_03
+pip install -r requirements.txt
+step_04 for jupyter notebook
+pip install -U ipykernel
