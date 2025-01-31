ebfdab1eb6b33bf6724793ec2abbaa2f87aba92b
IQRA JAHAGNIR <iqrajahangir698@gmail.com>
2025-01-31 19:36:04 +0500

    update files

--- Class_01(Data_Types)/Data_Types.py
+++ Class_01(Data_Types)/Data_Types.py
@@ -176,3 +176,131 @@ print(type(tup)) # type
 # # 4.Dictionary
 # dic={1:"2.3",2:"44"}
 # print(dic)
+
+
+# Type Conversion
+# You can convert from one type to another with the int(), float(), and complex() methods:
+
+# x = 1    # int
+# #convert from int to float:
+# a = float(x)
+# print(a,type(a))
+
+# y = 2.8  # float
+# #convert from float to int:
+# b = int(y)
+# print(b,type(b))
+
+# z = 1j   # complex
+# #convert from int to complex:
+# c = complex(x)
+# print(c,type(c))
+
+# Example 1 (int convert in float)
+# a=32   
+# print("Check type of",a,type(a))
+# b=float(a)
+# print("32 convert in float",b)
+# # Example 2 (float convert in int)
+# c=3.2
+# print("Check type of",c,type(c))
+# d=int(c)
+# print("Float convert in int",d)
+# # Example 3 (list Convert in set)
+# I=[3,5,10,5,2.5]
+# print("Check type of",I,type(I))
+# J=set(I)
+# print("list Convert in set",J)
+# # Example 4 (List convert in tople)
+# k=[3,5,10,5,2.5]
+# print("Check type of",k,type(k))
+# L=tuple(k)
+# print("List convert in tople =",L)
+# # Example 4 (tuple convert in list)
+# m=(3,5,10,5,2.5)
+# print("Check type of",m,type(m))
+# n=list(m)
+# print("tuple convert in list =",n)
+# # Example 5 (Set convert in list)
+# o={3,5,10,5,2.5}
+# print("Check type of",o,type(o))
+# p=list(o)
+# print("set convert in list =",p)
+
+# list 
+# name=[5,4.5,"Iqra"]
+# name[2]=23
+# name[0]="fatima"
+# print(name)
+
+# Ditionary
+# Ditionary={
+#     "FirstName":"Sheikh",
+#     "MiddleName":"Muhammad",
+#     "LastName":"Jahangir"}
+# print (Ditionary["FirstName"],Ditionary["MiddleName"],Ditionary["LastName"])
+
+# change value
+# Ditionary["FirstName"]="syed"
+# print (Ditionary["FirstName"],Ditionary["MiddleName"],Ditionary["LastName"])
+
+
+
+name=" Iqra Jahangir "
+message="Hello! world"
+# list=[2,4,5,6,7]
+# print(name.split())
+# print(len(list))
+# print(name.upper())
+# print(message.lower())
+# print(name.capitalize())
+# print(name.count('Iqra'))
+# print(name.find("h"))
+# print(message.replace("world","Universe"))
+# print(message.endswith("!"))
+# print(name.startswith(" "))
+
+# city="Karachi"
+# print(name.center(50))
+# print(f"My name is {name} \n I'm from {message}")
+
+
+# Days=str(input("Choose Days "))
+# match Days:
+#     case "Monday":
+#         print(f"Today's {Days}")
+#     case "Tuesday":
+#         print("Today's Tuesday")
+#     case "Wednesday":
+#         print("Today's Wednesday")
+#     case "Thursday":
+#         print("Today's Thursday")
+#     case "Friday":
+#         print("Today's Friday")
+#     case "Saturday":
+#         print("Today's Saturday")
+#     case "Sunday":
+#         print("Today's Sunday")
+#     case _:
+#         print("Invalid")
+
+
+# Days=str(input("Enter Days Name "))
+# s="Today's is "
+# match Days:
+#         case "Monday":
+#              print(s,"Monday")
+#         case "Tuesday":
+#             print(s,"Tuesday")
+#         case "Wednesday":
+#             print(s,"Wednesday")
+#         case "Thursday":
+#             print(s,"Thursday")
+#         case "Friday":
+#             print (s,"Friday")
+#         case "Saturday":
+#             print (s,"Saturday")
+#         case "Sunday":
+#             print(s,"Sunday")
+#         case _:
+#             print("Invalid Day")
--- Class_02(String,f-string)/Strings_f_Strings.py
+++ Class_02(String,f-string)/Strings_f_Strings.py
@@ -1,1 +1,17 @@
-print(10+20)
+# Slice Method
+# U="Hello World"
+# print(U[3:6])
+# print(len(U))
+# print(U[2:9:3])
+# print(U.count('World'))
+# print(U.find("World"))
+# print(U[6:10:2])
+
+
+# Unicode 
+# 1) Ord(Alphabets)
+# print(ord("C","d"))
+# 2) chr(Numberic)
+# print(chr(67))
+# 3) bin ()
+# print(bin(67))
